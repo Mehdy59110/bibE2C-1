3a6721477696bfafa6bb769dc8cbcd782fefe138
Deuteronome <o.burcker@e2c-grandlille.fr>
2025-06-17 11:47:07 +0200

    Intégration jeu dans html - game over à faire

--- script/test.js
+++ script/test.js
@@ -1,31 +1,43 @@
-let targetNumber = Math.floor(Math.random()*100)+1;
-console.log(targetNumber);
-let userNumber = prompt('Devinez un nombre entre 1 et 100 :');
+let trigger = document.querySelector('#send-button');
+let numberZone = document.querySelector('#user-number');
+let displayZone = document.querySelector('#game-display');
 
-while(userNumber != targetNumber) {
-    
-    if(userNumber > targetNumber) {
-        alert(`C'est trop grand!`);
+let targetNumber = Math.floor(Math.random()*100)+1;
+
+console.log(trigger, numberZone, displayZone,targetNumber);
+
+function compareNumber() {
+    //console.log("ça marche");
+    let userNumber = numberZone.value;
+    let result;
+    //console.log(userNumber);
+    if (userNumber==targetNumber) {
+        result='gagné';
+    } else if(userNumber > targetNumber) {
+        result=`C'est trop grand!`;
     } else {
-        alert(`C'est trop petit!`);
+        result=`C'est trop petit!`;
     }
 
-    userNumber = prompt('Devinez un nombre entre 1 et 100 :');
+    let resultZone = document.createElement('p');
+    resultZone.innerHTML = result;
+    resultZone.style.padding = "10px";
+    resultZone.classList.add('result-line');
+    
+
+    //console.log(resultZone);
+
+    displayZone.appendChild(resultZone);
+
 }
 
-alert('Bravo, vous n\'avez trouvé!');
+trigger.addEventListener('click', compareNumber);
 
 
 
 
 
 
-/*
-let firstname = prompt('Quel est votre prénom?');
-let lastname = prompt("Quel est votre nom?");
-
-alert(`Bonjour ${firstname} ${lastname}!`);
-*/
 
 
 
