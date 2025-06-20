65bb246b1db0ec7ea90eaa524d55807d7e34d984
Deuteronome <o.burcker@e2c-grandlille.fr>
2025-06-20 11:58:09 +0200

    Branche expérimentale

---
+++ script/game/menu.js
@@ -0,0 +1,17 @@
+export class Menu {
+    
+    menuZone;
+
+    constructor() {
+        this.menuZone = document.querySelector('#game-menu');
+    }
+
+    /**
+     * Ajoute le launcher du jeu dans le menu de jeu
+     * 
+     * @param {Instance d'un objet jeu} gameObject
+     */
+    setMenu(gameObject) {
+        
+    }
+}
